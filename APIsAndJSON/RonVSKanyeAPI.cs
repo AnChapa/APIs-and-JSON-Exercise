9c2a147dc4977416ad5796657bd2716679fb4c1e
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-02-15 15:33:47 -0600

    Cleaned up exercise 1

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,9 +1,11 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace APIsAndJSON
 {
@@ -11,29 +13,23 @@ namespace APIsAndJSON
     {
         public async Task GetConvo()
         {
-            try
-            {
-                using (var client = new HttpClient())
-                {
-                    var kanyeURL = "https://api.kanye.rest";
-                    var ronURL = " https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-                    for (int i = 0; i < 5; i++)
-                    {
-                        var kanyeResponse = await client.GetStringAsync(kanyeURL);
-                        var ronResponse = await client.GetStringAsync(ronURL);
+          using (var client = new HttpClient())
+          {
+              var kanyeURL = "https://api.kanye.rest";
+              var ronURL = " https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+              for (int i = 0; i < 5; i++)
+              {
+                  var kanyeResponse = await client.GetStringAsync(kanyeURL);
+                  var ronResponse = await client.GetStringAsync(ronURL);
 
-                        var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
-                        var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                  var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+                  var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
 
-                        Console.WriteLine($"Kanye : {kanyeQuote}");
-                        Console.WriteLine($"Ron : {ronQuote}");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.GetType().Name} - {ex.Message}");
-            }
+                  Console.WriteLine($"Kanye : \"{kanyeQuote}\"");
+                  Console.WriteLine($"Ron : {ronQuote}");
+                  Console.WriteLine("-----------------------------------");
+              }
+          }
         }
     }
 }
--- APIsAndJSON/obj/Debug/net6.0/APIsAndJSON.AssemblyInfo.cs
+++ APIsAndJSON/obj/Debug/net6.0/APIsAndJSON.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIsAndJSON")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a503a6b5034edb60939769c216560eb36199027")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c218ff19ebc82be5ce0d99de38134e8f4b33b783")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIsAndJSON")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIsAndJSON")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
